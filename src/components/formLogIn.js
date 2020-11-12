5f029eba19e197874fe48b5c1a5a4cc782e60053
fdsosa <fdsosa.35@gmail.com>
2020-11-12 15:48:08 -0300

    feature/form-validation - Finished forms validation

--- src/components/formLogIn.js
+++ src/components/formLogIn.js
@@ -1,45 +1,146 @@
 import '../style/form.scss';
-import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { React, Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
-function FormLogIn(props) {
-    const [emailLogIn, setEmailLogIn] = useState(''); 
-    const [passwordLogIn, setPasswordLogIn] = useState(''); 
+//REGEX
+const emailRegex = RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+//const nameRegex = RegExp(/^([a-zA-Z ]){2,30}$/);
 
-    const handleEmailChange = (e) => {
-        setEmailLogIn(e.target.value);
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
+
+  // validate form errors being empty
+  Object.values(formErrors).forEach(val => {
+    val.length > 0 && (valid = false);
+  });
+
+  // validate the form was filled out
+  Object.values(rest).forEach(val => {
+    val === null && (valid = false);
+  });
+
+  return valid;
+};
+
+const emptyValid = ({ formErrors, ...rest }) => {
+  let empty = false;
+
+  // validate the form was filled out
+  Object.values(rest).forEach(val => {
+    val === null && (empty = true);
+  });
+
+  return empty;
+};
+
+class FormLogIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emptyInputs: false,
+      email: null,
+      password: null,
+      formErrors: {
+        email: "",
+        password: ""
+      }
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let empty = emptyValid(this.state);
+    this.setState({ emptyInputs: empty });
+
+    if (formValid(this.state)) {
+      console.log(`
+            --SUBMITTING--
+            Name: ${this.state.name}
+            Email: ${this.state.email}
+          `);
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+      console.log(this.state)
+    }
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let formErrors = { ...this.state.formErrors };
+
+    switch (name) {
+      case "password":
+        formErrors.password =
+          value.length < 6 ? "Minimum 6 characaters required" : "";
+        break;
+      case "email":
+        formErrors.email = emailRegex.test(value)
+          ? ""
+          : "invalid email address";
+        break;
+      default:
+        break;
     }
 
-    const handlePasswordChange = (e) => {
-        setPasswordLogIn(e.target.value);
-    }
+    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+  };
 
-    const handleForm = () => {
-        console.log('Email: ' + emailLogIn);
-        console.log('Password: ' + passwordLogIn);
-    }
+  render() {
+    const { formErrors } = this.state;
 
-    return(
-        <div className="form-container">
-            <form>
-                <h1>Sign in to ChatApp</h1>
-                <div className="form-group">
-                    <div className="input-container">
-                        <input placeholder='Email' name='email' value={emailLogIn} id='email' onChange={handleEmailChange} type='email'></input>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="input-container">
-                        <input placeholder='Password' name='password' value={passwordLogIn} id='password' onChange={handlePasswordChange} type='password'></input>
-                    </div>
-                </div>
-                <div className="buttons-container buttons-container-one">
-                    <div className="button-container one-button">
-                        <button type="button" onClick={handleForm}>Sign In</button>
-                    </div>
-                </div>
-            </form>
-        </div>
+    return (
+      <div className="form-container">
+        <form onSubmit={this.handleSubmit} noValidate>
+          <h1>Sign in to ChatApp</h1>
+          <div className="form-group">
+            <div className="input-container">
+              <input 
+                placeholder='Email *' 
+                name='email' 
+                id='email'
+                className={formErrors.email.length > 0 || (this.state.emptyInputs && this.state.email === null) ? 'error' : ''} 
+                onChange={this.handleChange}
+                noValidate 
+                type='email'>
+              </input>
+            </div>
+            {formErrors.email.length > 0 && (
+              <small>{formErrors.email}</small>)
+            }
+          </div>
+          <div className="form-group">
+            <div className="input-container">
+              <input 
+                placeholder='Password *' 
+                name='password' 
+                id='password'
+                className={formErrors.password.length > 0 || (this.state.emptyInputs && this.state.password === null) ? 'error' : ''} 
+                onChange={this.handleChange}
+                noValidate 
+                type='password'>
+              </input>
+            </div>
+            {formErrors.password.length > 0 && (
+              <small>{formErrors.password}</small>)
+            }
+          </div>
+          {this.state.emptyInputs && (
+            <small className="empty-message"><FontAwesomeIcon className="user" icon={faInfoCircle} /> Please fill all the required inputs</small>
+          )}
+          <div className="buttons-container buttons-container-one">
+            <div className="button-container one-button">
+              <button type="submit">Sign In</button>
+            </div>
+          </div>
+          <p>New to ChatApp? <Link to="/signup">Create An Account</Link></p>
+        </form>
+      </div>
     );
+  }
 }
 
 export default FormLogIn;
--- src/components/formSignUp.js
+++ src/components/formSignUp.js
@@ -1,69 +1,209 @@
 import '../style/form.scss';
-import { React, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { React, Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+//REGEX
+const emailRegex = RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+//const nameRegex = RegExp(/^([a-zA-Z ]){2,30}$/);
 
-function FormSignUp(props) {
-    const [nameSign, setNameSign] = useState('');
-    const [emailSign, setEmailSign] = useState('');
-    const [passwordSign, setPasswordSign] = useState('');
-    const [rpasswordSign, setRpasswordSign] = useState('');
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
 
-    const handleForm = () => {
-        console.log(nameSign);
-        console.log(emailSign);
-        console.log(passwordSign);
-        console.log(rpasswordSign);
+  // validate form errors being empty
+  Object.values(formErrors).forEach(val => {
+    val.length > 0 && (valid = false);
+  });
+
+  // validate the form was filled out
+  Object.values(rest).forEach(val => {
+    val === null && (valid = false);
+  });
+
+  return valid;
+};
+
+const emptyValid = ({ formErrors, ...rest }) => {
+  let empty = false;
+
+  // validate the form was filled out
+  Object.values(rest).forEach(val => {
+    val === null && (empty = true);
+  });
+
+  return empty;
+};
+
+class FormSignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emptyInputs: false,
+      name: null,
+      email: null,
+      password: null,
+      rpassword: null,
+      formErrors: {
+        name: "",
+        email: "",
+        password: "",
+        rpassword: ""
+      }
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let empty = emptyValid(this.state);
+    this.setState({ emptyInputs: empty });
+
+    if (formValid(this.state)) {
+      console.log(`
+            --SUBMITTING--
+            Name: ${this.state.name}
+            Email: ${this.state.email}
+            Password: ${this.state.password}
+            Repeat Password: ${this.state.rpassword}
+          `);
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+      console.log(this.state)
+    }
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let formErrors = { ...this.state.formErrors };
+    let checkPassword = false;
+
+    switch (name) {
+      case "name":
+        formErrors.name =
+          value.length < 3 ? "Minimum 3 characaters required" : "";
+        break;
+      case "email":
+        formErrors.email = emailRegex.test(value)
+          ? ""
+          : "invalid email address";
+        break;
+      case "password":
+        formErrors.password =
+          value.length < 6 ? "Minimum 6 characaters required" : "";
+        checkPassword =
+          value.length > 5 ? true : false; 
+        break;
+      case "rpassword":
+        formErrors.rpassword =
+          value.length < 6 ? "Minimum 6 characaters required" : "";
+        checkPassword =
+          value.length > 5 ? true : false; 
+        break;
+      default:
+        break;
     }
 
-    const handleNameChange = (e) => {
-        setNameSign(e.target.value);
+    if (checkPassword) {
+      if (name === 'password') {
+        formErrors.rpassword = this.state.rpassword === e.target.value ? "" : "Passwords must match";
+        formErrors.password = formErrors.rpassword;
+      } else {
+        formErrors.password = this.state.password === e.target.value ? "" : "Passwords must match";
+        formErrors.rpassword = formErrors.password;
+      }
     }
 
-    const handleEmailChange = (e) => {
-        setEmailSign(e.target.value);
-    }
+    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+  };
 
-    const handlePasswordChange = (e) => {
-        setPasswordSign(e.target.value);
-    }
 
-    const handleRpasswordChange = (e) => {
-        setRpasswordSign(e.target.value);
-    }
+  render() {
+    const { formErrors } = this.state;
 
-    return(
-        <div className="form-container">
-            <form>
-                <h1>Create your account</h1>
-                <div className="form-group">
-                    <div className="input-container">
-                        <input placeholder='Name' name='name' value={nameSign} id='name' onChange={handleNameChange} type='text'></input>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="input-container">
-                        <input placeholder='Email' name='email' value={emailSign} id='email' onChange={handleEmailChange} type='email'></input>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="input-container">
-                        <input placeholder='Password' name='password' value={passwordSign} id='password' onChange={handlePasswordChange} type='password'></input>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="input-container">
-                        <input placeholder='Repeat Password' name='rpassword' value={rpasswordSign} id='rpassword' onChange={handleRpasswordChange} type='password'></input>
-                    </div>
-                </div>
-                <div className="buttons-container buttons-container-one">
-                    <div className="button-container one-button">
-                        <button type="button" onClick={handleForm}>Create Account</button>
-                    </div>
-                </div>
-                <p>New to ChatApp? <Link to="/signup">Create An Account</Link></p>
-            </form>
-        </div>
+    return (
+      <div className="form-container">
+        <form onSubmit={this.handleSubmit} noValidate>
+          <h1>Create your account</h1>
+          <div className="form-group">
+            <div className="input-container">
+              <input
+                className={formErrors.name.length > 0 || (this.state.emptyInputs && this.state.name === null) ? 'error' : ''}
+                placeholder='Name *'
+                name='name'
+                id='name'
+                onChange={this.handleChange}
+                noValidate
+                type='text'
+                required>
+              </input>
+            </div>
+            {formErrors.name.length > 0 && (
+              <small>{formErrors.name}</small>
+            )}
+          </div>
+          <div className="form-group">
+            <div className="input-container">
+              <input
+                className={formErrors.email.length > 0 || (this.state.emptyInputs && this.state.email === null) ? 'error' : ''}
+                placeholder='Email *'
+                name='email'
+                id='email'
+                onChange={this.handleChange}
+                noValidate
+                type='email'
+                required>
+              </input>
+            </div>
+            {formErrors.email.length > 0 && (
+              <small>{formErrors.email}</small>
+            )}
+          </div>
+          <div className="form-group">
+            <div className="input-container">
+              <input
+                className={formErrors.password.length || (this.state.emptyInputs && this.state.password === null) ? 'error' : ''}
+                placeholder='Password *'
+                name='password'
+                id='password'
+                onChange={this.handleChange}
+                noValidate
+                type='password'
+                required>
+              </input>
+            </div>
+            {formErrors.password.length > 0 && (
+              <small>{formErrors.password}</small>
+            )}
+          </div>
+          <div className="form-group">
+            <div className="input-container">
+              <input
+                className={formErrors.rpassword.length || (this.state.emptyInputs && this.state.rpassword === null) ? 'error' : ''}
+                placeholder='Repeat Password *'
+                name='rpassword'
+                id='rpassword'
+                onChange={this.handleChange}
+                noValidate
+                type='password'
+                required>
+              </input>
+            </div>
+            {formErrors.rpassword.length > 0 && (
+              <small>{formErrors.rpassword}</small>
+            )}
+          </div>
+          {this.state.emptyInputs && (
+            <small className="empty-message"><FontAwesomeIcon className="user" icon={faInfoCircle} /> Please fill all the required inputs</small>
+          )}
+          <div className="buttons-container buttons-container-one">
+            <div className="button-container one-button">
+              <button type="submit">Create Account</button>
+            </div>
+          </div>
+        </form>
+      </div>
     );
+  }
 }
 
 export default FormSignUp;
